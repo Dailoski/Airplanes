81bb5805ef817245e933b5e4206929eaa0da8bb0
Stefan Dailoski <dailoskistefan@gmail.com>
2019-11-22 04:07:24 +0100

    obrisan zarez

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
                 enforce: "pre",
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "eslint-loader",
+                loader: "eslint-loader"
             },
             {
                 test: /\.js$/,
